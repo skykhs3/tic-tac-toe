5d231d8722821568a4db05880b406da7805616cd
skykhs3 <skykhs3@naver.com>
2020-03-08 04:15:46 +0900

    히스토리 알려주기 직전

--- src/index.js
+++ src/index.js
@@ -4,24 +4,60 @@ import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+// class Square extends React.Component {
+
+//     render() {
+//       return (
+//         <button className="square" onClick={()=>this.props.onClick()}>
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            squares:Array(9).fill(null),
+            xIsNext:true,
+        }
+    }
+    handleClick(i) {
+        const{squares,xIsNext} = this.state;
+        //xIsNext:!xIsNext;
+        console.log(i,squares[i]);
+        
+       
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+          }
+        else{
+            squares[i] = this.state.xIsNext==true?'X':'O';
+            this.setState({squares: squares, xIsNext: !xIsNext});
+        }
+    }
+    
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>;
     }
   
     render() {
-      const status = 'Next player: X';
-  
+        const winner = calculateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
       return (
         <div>
           <div className="status">{status}</div>
@@ -64,5 +100,28 @@ class Square extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById('root'));
 
-
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+    //   console.log(a);
+    //   a=-a;
+    //   console.log(a);
+    //   a=-a;
+    //   console.log(a);
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return false;
+  }
 serviceWorker.unregister();
