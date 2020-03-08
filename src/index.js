90e043ca5df83df1a2918b79eaf45cf22332813e
skykhs3 <skykhs3@naver.com>
2020-03-08 19:46:53 +0900

    빨간색 표기
    승자가 이겼을 때 뭐 때문에 이겼는지 표기해줌

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ import * as serviceWorker from './serviceWorker';
 //     }
 //   }
 function Square(props) {
+
+  if(props.isEnd && (props.id==props.a || props.id==props.b || props.id==props.c) ){
+    return(
+    <button style={{color:'red'}}className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+    );
+  }
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
@@ -31,17 +39,17 @@ class Board extends React.Component {
   //     }
   // }
 
-
+  
   renderSquare(i) {
     
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square id ={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)}
+    a={this.props.a} b={this.props.b} c={this.props.c} isEnd={this.props.isEnd} />;
   }
 
   render() {
- 
+    console.log('winner:'+this.props.isEnd);
     return (
       <div>
-
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -70,10 +78,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
+
     };
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -85,29 +95,54 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber:history.length,
+    });
+  }
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const square = current.squares.slice();
     const winner = calculateWinner(current.squares);
+    const abc=calculateWinner2(current.squares);
+
+    const moves = history.map((step, index) => {
+      const desc = index ?
+        'Go to move #' + index :
+        'Go to game start';
+      return (
+        <li key={index}>
+          <button onClick={() => this.jumpTo(index)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
+      if(this.state.stepNumber==9){
+        status='There is no winner';
+      }
+      else{
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
     }
     
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
+          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} 
+          isEnd={winner} a={abc[0]} b={abc[1]} c={abc[2]}/>
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <div> <u>{status}</u></div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -130,15 +165,29 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    //   console.log(a);
-    //   a=-a;
-    //   console.log(a);
-    //   a=-a;
-    //   console.log(a);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
   return false;
+}
+function calculateWinner2(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [a,b,c];
+    }
+  }
+  return [];
 }
 serviceWorker.unregister();
